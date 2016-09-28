701177e429bccd73b66e4a1a9f0144197d228609
iykzhang <iykzhang@gmail.com>
2016-09-28 14:53:59 +0800

    修改

--- 3/subString.c
+++ 3/subString.c
@@ -2,7 +2,7 @@
 #include<malloc.h>
 int lengthOfLongestSubstring(char* s);
 int main(){
-	char *a="pwwkew";
+	char *a="abcabcbb";
 	int r=lengthOfLongestSubstring(a);
 	printf("%d\n",r);
 	return 0;
@@ -11,31 +11,26 @@ int lengthOfLongestSubstring(char* s) {
 	int i=0;
 	int maxLength=0;
 	int currentLength=0;
-	printf("%s\n",s);
 	int hasGoneNum=0;
 	currentLength=0;
 	while(s[i]!='\0'){
-		printf("s[%d]=%c\n",i,s[i]);
 		int n=hasGoneNum;
 		int isHasSame=0;
 		for(;n<i;n++){
 			if(s[n]==s[i]){
 				isHasSame=1;
-				currentLength=0;
+				currentLength=i-n;
 				hasGoneNum=n+1;
-				i=hasGoneNum;
-				printf("hasGoneNum=%d\n",hasGoneNum);
 				break;
 			}
 		}
 		if(!isHasSame){
-			i++;
 			currentLength++;
-			if(maxLength<currentLength){
-				maxLength=currentLength;
-				printf("max=%d\n",maxLength);
-			}
 		}
+		if(maxLength<currentLength){
+			maxLength=currentLength;
+		}
+		i++;
 	}
 	return maxLength;
 }
