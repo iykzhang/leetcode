f6ce987d677c6c2b2b0f8ea87135906754a51052
iykzhang <iykzhang@gmail.com>
2016-09-29 11:24:41 +0800

    addTowNum

--- 2/add_two_number.c
+++ 2/add_two_number.c
@@ -34,3 +34,38 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
 		return head;
 	}
 }
+
+struct ListNode* addTwoNumbers2(struct ListNode* l1, struct ListNode* l2) {
+	if(l1==NULL) return l2;
+	if(l2==NULL) return l1;
+
+	struct ListNode *head;
+	head=l1;
+	int h=0;
+	while((l1!=NULL&&h)||l2!=NULL){
+		int d1=l1==NULL?0:l1->val;
+		int d2=l2==NULL?0:l2->val;
+		int total=d1+d2+h;
+		int result=total%10;
+		h=total/10;
+		if(l1!=NULL){
+			l1->val=result;
+			if(l2==NULL&&!h){
+				return head;
+			}
+			if(l1->next==NULL){
+				if((l2==NULL||l2->next==NULL)&&!h){
+					return head;
+				}
+				struct ListNode* node=(struct ListNode*)malloc(sizeof(struct ListNode));
+				node->next=NULL;
+				node->val=0;
+				l1->next=node;
+			}
+		}
+
+		if(l1!=NULL)l1=l1->next;
+		if(l2!=NULL)l2=l2->next;
+	}
+	return head;
+}
